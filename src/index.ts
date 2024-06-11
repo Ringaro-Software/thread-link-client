ed9ff249deeb21e7162f86202eeffe0f6d607ae3
Vlad Rotaru <cuzzea@gmail.com>
2024-06-11 08:10:37 +0200

    chore: Update package name and version to thread-link 1.0.4

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './admin';
+export * from './oas';
+export * from './public';
---
+++ src/public/index.ts
@@ -0,0 +1,5 @@
+export * from './messages';
+export * from './messageTemplates';
+export * from './publicClient';
+export * from './threads';
+export * from './topics';
